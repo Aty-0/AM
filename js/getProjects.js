b6d741d380644c3e62bb0f1a72ed7f1085c698aa
A_T <danich-anton-live@yandex.ru>
2026-04-16 22:16:33 +0300

    Improve image container logic

--- js/getProjects.js
+++ js/getProjects.js
@@ -19,7 +19,7 @@ function appendNewPostContainer(root, element, index) {
     </div>`);
 
     const postContainer = document.getElementById(`post-container-${index}`);
-    appendImageContainer(postContainer, element.imgs, index);
+    appendImageContainer(postContainer, element.name, element.imgs);
     appendLink(postContainer, element);
 }
 
@@ -33,64 +33,98 @@ function appendLink(postContainer, element) {
         : /*html*/`<div class="label-small">Ссылка на проект недоступна, так как проект является приватным.</div>`));
 }
 
-function appendImageContainer(postContainer, imgs, index) {    
+function appendImageContainer(postContainer, name, imgs) {    
+    // Don't do anything if imgs array is empty
     if (imgs.length === 0) {
         return;
     }
-    const firstImage = imgs[0];    
-    const imageContainerHtml = /*html*/`
-    <div class="image-container">
-        ${ appendImage(index, firstImage) }
-        ${ imgs.length > 1 ? 
-            /*html*/`<div id="button-next-${index}" class="image-arrow image-arrow-right"></div> 
-            <div id="button-back-${index}" class="image-arrow image-arrow-left"></div>` 
-        : "" }    
-    </div>
-    <div id="label-image-index-${index}" class="label-small">1/${imgs.length}</div>
-    `;
 
+    // Create bunch of images 
+    let imagesHtml = '';
+    imgs.forEach((img, idx) => {
+        imagesHtml += appendImage(name, idx, img);
+    });
+
+    const imageContainerHtml = /*html*/`
+    <div class="image-container" id="image-container-${name}">
+        ${imagesHtml}
+        ${imgs.length > 1 ? 
+            /*html*/`<div id="button-next-${name}" class="image-arrow image-arrow-right"></div> 
+            <div id="button-back-${name}" class="image-arrow image-arrow-left"></div>` 
+        : ""}    
+    </div>
+    <div id="label-image-index-${name}" class="label-small">1/${imgs.length}</div>
+    `;
+    
     postContainer.insertAdjacentHTML("beforeend", imageContainerHtml);
 
+    // Show first image
+    showImageByIndex(imgs, name, 0);
+
+    // Add Next-Back buttons
     if (imgs.length > 1) {
-        const backButton = document.getElementById(`button-back-${index}`);
-        backButton.onclick = () => changeImage(imgs, index, -1);
+        const backButton = document.getElementById(`button-back-${name}`);
+        if (backButton) {
+            backButton.onclick = () => changeImage(imgs, name, -1);
+        }
     
-        const nextButton = document.getElementById(`button-next-${index}`);
-        nextButton.onclick = () => changeImage(imgs, index, 1);
+        const nextButton = document.getElementById(`button-next-${name}`);
+        if (nextButton) {
+            nextButton.onclick = () => changeImage(imgs, name, 1);
+        }
     }
 }
 
-function changeImage(imgs, postIndex, moveTo) {
-    const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+const clamp = (value, min, max) => Math.min(max, Math.max(min, value));
+function changeImage(imgs, name, moveTo) {
+    // Get current index from label
+    const indexLabel = document.getElementById(`label-image-index-${name}`);
+    if (!indexLabel) {
+        return;
+    }
 
-    const postImage = document.getElementById(`post-image-${postIndex}`);
-    const currentImageIndex = Number(postImage.getAttribute("imgIndex"));
+    const currentText = indexLabel.innerHTML;
+    const currentImageIndex = parseInt(currentText.split('/')[0]) - 1;
     const nextImageIndex = clamp(currentImageIndex + moveTo, 0, imgs.length - 1);
+
     if (currentImageIndex === nextImageIndex) {
         return;
     }
 
-    const nextImage = imgs[nextImageIndex];
-    if (nextImage === undefined) {
-        console.error("nextImage is undefined");
+    showImageByIndex(imgs, name, nextImageIndex);
+}
+
+function showImageByIndex(imgs, name, index) {
+    const container = document.getElementById(`image-container-${name}`);
+    if (!container) {
         return;
+    } 
+            
+    // Get all image wrappers
+    const wrappers = container.querySelectorAll('.image-wrapper');    
+    wrappers.forEach(wrapper => {
+        wrapper.style.display = 'none';
+    });
+    
+    // Show selected wrapper
+    if (wrappers[index]) {
+        wrappers[index].style.display = 'block';
     }
-    postImage.setAttribute("imgIndex", nextImageIndex);
-    postImage.src = nextImage.img;
-
-    const indexLabel = document.getElementById(`label-image-index-${postIndex}`);
-    indexLabel.innerHTML = `${nextImageIndex + 1}/${imgs.length}`;
-
-    const postImageHint = document.getElementById(`post-image-hint-${postIndex}`);
-    if (postImageHint) {
-        postImageHint.innerHTML = nextImage.hint; 
+    
+    // Update index label
+    const indexLabel = document.getElementById(`label-image-index-${name}`);
+    if (indexLabel) {
+        indexLabel.innerHTML = `${index + 1}/${imgs.length}`;
     }
 }
 
-function appendImage(postIndex, img) {
-    if (img === undefined) {
+function appendImage(name, imageIndex, img) {
+    if (img === undefined || !img.img) {
         return "";
     }
-    return /*html*/`<img id="post-image-${postIndex}" imgIndex="0" src=${img.img} alt=${img.img}></img>` 
-    + (img.hint ? /*html*/`<div id="post-image-hint-${postIndex}" class="label-extra-small">${img.hint}</div>` : "");
+    
+    return /*html*/`<div class="image-wrapper" style="display: none;">
+        <img id="post-image-${name}-${imageIndex}" class="post-image" imgIndex="${imageIndex}" src="${img.img}" alt="${img.alt || img.img}">
+        ${img.hint ? /*html*/`<div id="post-image-hint-${name}-${imageIndex}" class="label-extra-small">${img.hint}</div>` : ""}
+    </div>`;
 }
